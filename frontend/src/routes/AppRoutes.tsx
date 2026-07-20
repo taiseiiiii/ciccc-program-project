e1c9faab6b1f473b72e066377737fd363ef3530e
Suzuna Arakaki <suzuna.arakaki@gmail.com>
2026-07-19 21:41:55 -0700

    feat: implement responsive split layout and forms for Auth page

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -5,10 +5,12 @@ import Progress from "../pages/Progress";
 import AICoach from "../pages/AICoach";
 import AppLayout from "../layouts/AppLayout";
 import Profile from "../pages/Profile";
+import AuthLayout from "../layouts/AuthLayout";
 
 const AppRoutes = () => {
   return (
     <Routes>
+      <Route path="/auth" element={<AuthLayout />}></Route>
       <Route element={<AppLayout />}>
         <Route index element={<Dashboard />} />
         <Route path="log-session" element={<LogSession />} />
