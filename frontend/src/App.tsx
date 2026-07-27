a99a2214d4b0196b07be956f2e59245df3897b0b
Suzuna Arakaki <suzuna.arakaki@gmail.com>
2026-07-27 13:11:55 -0700

    feat: setup supabase auth, route guards, and api client

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "./context/ThemeContext";
-import { AuthProvider } from "./context/ AuthContext";
+import { AuthProvider } from "./context/AuthContext";
 import AppRoutes from "./routes/AppRoutes";
 import { Toaster } from "react-hot-toast";
 
--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,4 +1,5 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate, Outlet } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
 import Dashboard from "../pages/Dashboard";
 import LogSession from "../pages/LogSession";
 import Progress from "../pages/Progress";
@@ -7,16 +8,43 @@ import AppLayout from "../layouts/AppLayout";
 import Profile from "../pages/Profile";
 import AuthLayout from "../layouts/AuthLayout";
 
+const RequireAuth = () => {
+  const { session, loading } = useAuth();
+  if (loading) return null;
+
+  if (session) {
+    return <Outlet />;
+  } else {
+    return <Navigate to={"/auth"} replace />;
+  }
+};
+
+const RedirectIfAuthed = () => {
+  const { session, loading } = useAuth();
+  if (loading) return null;
+
+  if (session) {
+    return <Navigate to={"/"} replace />;
+  } else {
+    return <Outlet />;
+  }
+};
+
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/auth" element={<AuthLayout />}></Route>
-      <Route element={<AppLayout />}>
-        <Route index element={<Dashboard />} />
-        <Route path="log-session" element={<LogSession />} />
-        <Route path="progress" element={<Progress />} />
-        <Route path="ai-coach" element={<AICoach />} />
-        <Route path="profile" element={<Profile />} />
+      <Route element={<RedirectIfAuthed />}>
+        <Route path="/auth" element={<AuthLayout />} />
+      </Route>
+
+      <Route element={<RequireAuth />}>
+        <Route element={<AppLayout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="log-session" element={<LogSession />} />
+          <Route path="progress" element={<Progress />} />
+          <Route path="ai-coach" element={<AICoach />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
       </Route>
     </Routes>
   );
